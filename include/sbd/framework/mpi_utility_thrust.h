5ff83df4a19172dcb420cecb5520d4859a7f121b
Ryo Wakizaka <ryo.wakizaka@ibm.com>
2026-03-23 10:45:57 +0900

    Add a preprocessor definition to avoid passing thrust::device_vector to MPI_Allreduce

--- include/sbd/framework/mpi_utility_thrust.h
+++ include/sbd/framework/mpi_utility_thrust.h
@@ -12,6 +12,8 @@
 namespace sbd
 {
 
+#ifdef SBD_THRUST_SAFE_MPI_ALLREDUCE
+
 template <typename ElemT>
 void MpiAllreduce(thrust::device_vector<ElemT> &A, MPI_Op op, MPI_Comm comm)
 {
@@ -23,6 +25,21 @@ void MpiAllreduce(thrust::device_vector<ElemT> &A, MPI_Op op, MPI_Comm comm)
     MPI_Allreduce(h_send.data(), h_recv.data(), static_cast<int>(h_send.size()), DataT, op, comm);
     thrust::copy(h_recv.begin(), h_recv.end(), A.begin());
 }
+
+#else
+
+template <typename ElemT>
+void MpiAllreduce(thrust::device_vector<ElemT> &A, MPI_Op op, MPI_Comm comm)
+{
+    std::cout << "   TEST MpiAllreduce" << std::endl;
+    // Calling MPI functions directly on the `device_vector` sometimes caused instability,
+    // so copy them to the host temporarily.
+    MPI_Datatype DataT = GetMpiType<ElemT>::MpiT;
+    thrust::device_vector<ElemT> B(A);
+    MPI_Allreduce((ElemT *)thrust::raw_pointer_cast(B.data()), (ElemT *)thrust::raw_pointer_cast(A.data()), A.size(), DataT, op, comm);
+}
+
+#endif // SBD_THRUST_SAFE_MPI_ALLREDUCE
 
 template <typename ElemT>
 void _MpiSlide(const ElemT* A,
